4275cfc84e7d6f2fb85b13dcb019841ee806bd00
setalias <sandy@sandylee.me>
2021-08-03 18:57:18 -0500

    pw encrypt formatting

--- mss/pw_encrypt.py
+++ mss/pw_encrypt.py
@@ -4,14 +4,17 @@ from passlib.context import CryptContext
 
 # create context with PBKDF2
 pwd_context = CryptContext(
-        schemes=["pbkdf2_sha256"],
-        default="pbkdf2_sha256",
-        pbkdf2_sha256__default_rounds=30000 # -!- if wait time is too long reduce number of rounds -!-
+    schemes=["pbkdf2_sha256"],
+    default="pbkdf2_sha256",
+    pbkdf2_sha256__default_rounds=30000  # -!- if wait time is too long reduce number of rounds -!-
 )
+
 
 # encrypt
 def encrypt_password(password):
     return pwd_context.encrypt(password)
+
+
 # -- TO ENCRYPT--
 # before storing to database:
 #       encrypt_password()
