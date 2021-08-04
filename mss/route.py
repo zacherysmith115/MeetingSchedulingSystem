5e653f0cdfa6f0825416755a428e5f7c8d783ae1
setalias <sandy@sandylee.me>
2021-08-03 21:10:10 -0500

    spelling

--- mss/route.py
+++ mss/route.py
@@ -5,12 +5,13 @@ from mss.forms import *
 from mss import app, db
 from mss.models import *
 from flask_login import login_user, current_user, logout_user, login_required
-from sqlalchemy.orm.collections import InstrumentedList 
+from sqlalchemy.orm.collections import InstrumentedList
+
 
 # contains all the routing scripts to navigate the application #
 
 # Login routing method
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
@@ -20,21 +21,22 @@ def login():
         # Query the db for email
         user = User.query.filter(User.email == form.email.data).first()
 
-        # Ensure email is in the db and submited password matches password on record
-        if(user and user.password == form.password.data):
+        # Ensure email is in the db and submitted password matches password on record
+        if user and user.password == form.password.data:
             login_user(user, remember=False)
 
             # Route to client home page
-            if(isinstance(user, Client)):
+            if isinstance(user, Client):
                 return redirect(url_for('dashboard'))
 
             # Route to admin home page
-            if(isinstance(user, Admin)):
+            if isinstance(user, Admin):
                 return redirect(url_for('adminDashboard'))
         else:
-            flash('Login unsuccesful', 'danger')
-    # Credentials failed, resumbit Login page
+            flash('Login unsuccessful', 'danger')
+    # Credentials failed, resubmit Login page
     return render_template('Login.html', form=form)
+
 
 @app.route('/Logout')
 def logout():
@@ -43,59 +45,56 @@ def logout():
 
 
 # Create account routing method
-@app.route("/CreateAccount", methods = ['GET', 'POST'])
+@app.route("/CreateAccount", methods=['GET', 'POST'])
 def createAccount():
     form = CreateAccountForm()
     if form.validate_on_submit():
         if '@pss.com' in form.email.data:
-            client = Client(first_name = form.first_name.data, last_name = form.last_name.data, email = form.email.data, password = form.password.data)
+            client = Client(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data,
+                            password=form.password.data)
             db.session.add(client)
             db.session.commit()
-            flash('Account succesfully created for ' + form.first_name.data + ' ' + form.last_name.data, 'success')
+            flash('Account successfully created for ' + form.first_name.data + ' ' + form.last_name.data, 'success')
             return redirect(url_for('dashboard'))
         else:
             flash('Must use a valid compnay email', 'danger')
-    return render_template('CreateAccount.html', form = form)
-
+    return render_template('CreateAccount.html', form=form)
 
 
 # Client dashboard routing method
-@app.route('/Dashboard', methods = ['GET'])
+@app.route('/Dashboard', methods=['GET'])
 @login_required
 def dashboard():
-
     # Build meetings list to add to calendar
     meetings = []
     if type(current_user.meetings_participant) is InstrumentedList:
-        meetings.extend(current_user.meetings_participant) 
+        meetings.extend(current_user.meetings_participant)
     else:
         meetings.append(current_user.meetings_participant)
- 
+
     if type(current_user.meetings_creator) is InstrumentedList:
         meetings.extend(current_user.meetings_creator)
     else:
-         meetings.append(current_user.meetings_creator)
-
+        meetings.append(current_user.meetings_creator)
 
     meeting_events = []
     for meeting in meetings:
-        meeting_events.append({'title' : meeting.title,
-                               'start' : meeting.start_time,
-                               'end' : meeting.end_time})
-    
+        meeting_events.append({'title': meeting.title,
+                               'start': meeting.start_time,
+                               'end': meeting.end_time})
 
-    return render_template('Dashboard.html', events = meeting_events)
-
+    return render_template('Dashboard.html', events=meeting_events)
 
 
 # Client my meetings routing method
-@app.route('/MyMeetings', methods = ['GET', 'POST'])
+@app.route('/MyMeetings', methods=['GET', 'POST'])
 @login_required
 def myMeetings():
     return render_template('MyMeetings.html')
 
+
 # Client edit account routing method
-@app.route('/EditAccount', methods = ['GET', 'POST'])
+@app.route('/EditAccount', methods=['GET', 'POST'])
 @login_required
 def editAccount():
     form = EditAccountForm()
@@ -104,7 +103,7 @@ def editAccount():
         form.first_name.data = current_user.first_name
         form.last_name.data = current_user.last_name
         form.email.data = current_user.email
-        return render_template('EditAccount.html', form = form)
+        return render_template('EditAccount.html', form=form)
 
     if form.validate_on_submit():
         user = User.query.filter(User.id == current_user.id).first()
@@ -114,19 +113,19 @@ def editAccount():
         user.password = form.password.data
         print(user)
         db.session.commit()
-        return render_template('EditAccount.html', form = form)
+        return render_template('EditAccount.html', form=form)
 
+    return render_template('EditAccount.html', form=form)
 
-    return render_template('EditAccount.html', form = form)
 
 # Client add payment info routing method
-@app.route('/EditAccount/PaymentInfo', methods = ['GET', 'POST'])
+@app.route('/EditAccount/PaymentInfo', methods=['GET', 'POST'])
 @login_required
 def addPaymentInfo():
     form = PaymentInfoForm()
     card = current_user.card
 
-    #If already has a card populate some of the information
+    # If already has a card populate some of the information
     if not form.validate_on_submit():
         if card is not None:
             form.card_name.data = card.name
@@ -138,23 +137,23 @@ def addPaymentInfo():
             month_str = str(card.exp_date.month)
             if len(month_str) != 2:
                 month_str = '0' + month_str
-            
+
             year_str = str(card.exp_date.year)
             year_str = year_str[2:]
 
             form.card_exp_date.data = month_str + '/' + year_str
 
-            return render_template('PaymentInfo.html', form = form)
+            return render_template('PaymentInfo.html', form=form)
 
     # form is validated on submit record to db
     if form.validate_on_submit():
         exp_date_str = form.card_exp_date.data
-        date = datetime(year = int('20' + exp_date_str[3:]), month = int(exp_date_str[0:2]), day=1)
+        date = datetime(year=int('20' + exp_date_str[3:]), month=int(exp_date_str[0:2]), day=1)
 
         # No previous associated card information
         if card is None:
-            card = Card(client_id = current_user.id, name = form.card_name.data,
-                    number = form.card_number.data, exp_date = date, ccv = form.card_ccv.data)
+            card = Card(client_id=current_user.id, name=form.card_name.data,
+                        number=form.card_number.data, exp_date=date, ccv=form.card_ccv.data)
             db.session.add(card)
         else:
             card = Card.query.filter(Card.client_id == current_user.id).first()
@@ -162,63 +161,67 @@ def addPaymentInfo():
             card.number = form.card_number.data
             card.exp_date = date
             card.ccv = form.card_ccv.data
-            
-       
+
         db.session.commit()
 
         flash('Payment info updated', 'success')
-        return render_template('PaymentInfo.html', form = form)
-    
-    return render_template('PaymentInfo.html', form = form)
+        return render_template('PaymentInfo.html', form=form)
+
+    return render_template('PaymentInfo.html', form=form)
+
 
 # Client ticket center routing method
-@app.route('/TicketCenter', methods =['GET', 'POST'])
+@app.route('/TicketCenter', methods=['GET', 'POST'])
 @login_required
 def ticketCenter():
     return render_template('TicketCenter.html')
 
+
 # Client ticket center routing method
-@app.route('/Help', methods =['GET', 'POST'])
+@app.route('/Help', methods=['GET', 'POST'])
 @login_required
 def help():
     return render_template('Help.html')
 
 
-
 # Admin dashboard routing method
-@app.route('/AdminDashboard', methods = ['GET'])
+@app.route('/AdminDashboard', methods=['GET'])
 @login_required
 def adminDashboard():
     return render_template('AdminDashboard.html')
 
+
 # Admin edit account routing method
-@app.route('/AdminEditAccount', methods = ['GET', 'POST'])
+@app.route('/AdminEditAccount', methods=['GET', 'POST'])
 @login_required
 def adminEditAccount():
     form = EditAccountForm()
-    return render_template('AdminEditAccount.html', form = form)
+    return render_template('AdminEditAccount.html', form=form)
+
 
 # Admin ticket center routing method
-@app.route('/AdminTicketCenter', methods = ['GET', 'POST'])
+@app.route('/AdminTicketCenter', methods=['GET', 'POST'])
 @login_required
 def adminTicketCenter():
     return render_template('AdminTicketCenter.html')
 
+
 # Admin display meetings routing method
-@app.route('/AdminDisplayMeeetings', methods =['GET', 'POST'])
+@app.route('/AdminDisplayMeeetings', methods=['GET', 'POST'])
 @login_required
 def adminDisplayMeetings():
     return render_template('AdminDisplayMeetings.html')
 
+
 # Admin edit admin accounts routing method
-@app.route('/AdminEditAdminAccounts', methods =['GET', 'POST'])
+@app.route('/AdminEditAdminAccounts', methods=['GET', 'POST'])
 @login_required
 def adminEditAdminAccounts():
     return render_template('AdminEditAdminAccounts.html')
 
 
 # Admin edit rooms routing method
-@app.route('/AdminEditRooms', methods =['GET', 'POST'])
+@app.route('/AdminEditRooms', methods=['GET', 'POST'])
 @login_required
 def adminEditRooms():
     delform = DelRoomForm()
@@ -227,8 +230,7 @@ def adminEditRooms():
 
 
 # Admin update bill routing method
-@app.route('/AdminUpdateUserBill', methods =['GET', 'POST'])
+@app.route('/AdminUpdateUserBill', methods=['GET', 'POST'])
 @login_required
 def adminUpdateUserBill():
     return render_template('AdminUpdateUserBill.html')
-
