5aa5f4b8f091165c5fdb4732920a532a510d00d6
setalias <sandy@sandylee.me>
2021-08-05 22:17:53 -0500

    update with main

--- mss/forms.py
+++ mss/forms.py
@@ -63,14 +63,16 @@ class AddRoomForm(FlaskForm):
     add_room = StringField('Add Room', validators=[DataRequired(), Length(max=20)])
     submit = SubmitField('Add Room Submit')
 
+
 class CreateMeetingForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(max=120)])
-    date = DateField('Date', format ='%m/%d/%y',validators=[DataRequired()])
+    date = DateField('Date', format='%m/%d/%y', validators=[DataRequired()])
     start_time = SelectField('Start Time', coerce=str)
-    end_time =  SelectField('End Time', coerce=str)
+    end_time = SelectField('End Time', coerce=str)
     description = TextAreaField('Description')
 
     submit = SubmitField('Submit')
+
 
 class UpdateUserBill(FlaskForm):
     client_id = StringField('Client ID', validators=[DataRequired(), Length(max=20)])
